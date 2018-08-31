5859c9457ab6f461af3093068e4bb46b640943a9
jokeefe1 <jokeefe@weegoglobal.com>
2018-08-30 23:29:38 -0400

    single page store with reducers, actions, and action generators
    Complete redux store first draft

--- src/playground/ReduxApp.js
+++ src/playground/ReduxApp.js
@@ -1,4 +1,122 @@
 import React from "react";
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
+
+//Action Generators
+
+//ADD_EXPENSE
+const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0
+}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+//REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+});
+//EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  id,
+  updates
+});
+//SET_TEXT_FILTER
+const setTextFilter = (text = "") => ({
+  type: "SET_TEXT_FILTER",
+  text
+});
+//SORT_BY_DATE
+const sortByDate = () => ({
+  type: "SORT_BY_DATE"
+});
+//SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: "SORT_BY_AMOUNT"
+});
+//SET_START_DATE
+const setStartDate = setStartDate => ({
+  type: "SET_START_DATE",
+  setStartDate
+});
+//SET_END_DATE
+const setEndDate = setEndDate => ({
+  type: "SET_END_DATE",
+  setEndDate
+});
+
+const expensesDefaults = [];
+const filtersDefaults = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+//Reducers
+const expenseReducer = (state = expensesDefaults, action) => {
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map(expense => {
+        return expense.id === action.id
+          ? { ...expense, ...action.updates }
+          : expense;
+      });
+    default:
+      return state;
+  }
+};
+const filtersReducer = (state = filtersDefaults, action) => {
+  switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return { ...state, text: action.text };
+    case "SORT_BY_DATE":
+      return { ...state, sortBy: "date" };
+    case "SORT_BY_AMOUNT":
+      return { ...state, sortBy: "amount" };
+    case "SET_START_DATE":
+      return { ...state, startDate: action.setStartDate };
+    case "SET_END_DATE":
+      return { ...state, endDate: action.setEndDate };
+    default:
+      return state;
+  }
+};
+
+//Create Store
+const store = createStore(combineReducers({ expenseReducer, filtersReducer }));
+//Subscribe to redux store
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+//Actions
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 300 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Coffee", amount: 900 })
+);
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 501 }));
+// store.dispatch(setTextFilter({ text: 'School' }))
+// store.dispatch(sortByDate(10))
+// store.dispatch(sortByAmount(110))
+// store.dispatch(setStartDate(12))
+// store.dispatch(setEndDate(14))
+
+// console.log(store.getState())
 
 const ReduxApp = () => <div>ReduxApp</div>;
 
